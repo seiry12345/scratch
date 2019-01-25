b4b180ff1e2ba35d36f1f28a64d355c577dcbf3f
seiry12345 <seiry@mail.ru>
2019-01-25 22:24:46 +0600

    second

--- .history/js/live-search_20190125213944.js
+++
@@ -1,9 +0,0 @@
-$("#search").keyup(function () {
-		_this = this;
-		$.each($("table tr, table td, table th"), function () {
-			if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
-				$(this).hide();
-			else
-				$(this).show();
-		});
-	});
