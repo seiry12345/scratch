61d279321f2328943938b43d221abc5c55faad42
seiry12345 <35530095+seiry12345@users.noreply.github.com>
2019-01-29 10:35:28 +0600

    Update ajax.php

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,28 +1,53 @@
 https://codepen.io/seiry12345/pen/RqYKVJ
+https://swiftmailer.symfony.com/docs/introduction.html
 
-if (!empty($_POST)) {
-    $email = trim($_POST['email']);
-    $name = htmlspecialchars($_POST['name']);
-    $text = htmlspecialchars($_POST['text']);
-    $phone = htmlspecialchars($_POST["phone"]);
+<?php
 
-    $to = 'test@mail.ru';
-    $subject = 'Новая заявка с сайта' . $_SERVER['SERVER_NAME'];
-    $message = '
-                <html>
-                    <head>
-                        <title>' . $subject . '</title>
-                    </head>
-                    <body>
-                        <p>Имя: ' . $name . '</p>
-						<p>Телефон: ' . $phone . '</p>
-						<p>Почта: ' . $email . '</p>
-                        <p>Сообщение: ' . $text . '</p>
-                    </body>
-                </html>';
-    $headers = "Content-type: text/html; charset=utf-8 \r\n";
-    $headerss.="Date: ".date('D, d M Y h:i:s')."\r\n";
-    $headers .= "From: Отправитель <from@example.com>\r\n";
-    mail($to, $subject, $message, $headers, $headerss);
+include_once '../../vendor/autoload.php';
+
+const OWNER_EMAIL = '';
+const DEVELOP_EMAIL = '';
+const DEVELOP_IP = "";
+
+if (isset($_POST)) {
+
+  if ($_SERVER['REMOTE_ADDR'] == DEVELOP_IP) {
+    $sendmailto = DEVELOP_EMAIL;
+  } else {
+    $sendmailto = OWNER_EMAIL;
+  }
+
+  $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+  $formDate = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
+  $subject = 'Новая заявка с сайта' . $_SERVER['SERVER_NAME'];
+  $msg = 'Телефон: ' . $phone . '<br>'
+      . 'Удобное время: ' . $formDate;
+
+
+// Create a message
+  $message = (new Swift_Message($subject))
+      ->setFrom(array('' => 'Почтовый робот'))
+      ->setTo($sendmailto)
+      ->setBody($msg, 'text/html');
+
+  $message->setBcc(['', '']);
+
+  $transport = (new Swift_SmtpTransport('smtp.yandex.ru', 587, 'tls'))
+      ->setUsername('')
+      ->setPassword('');
+
+  $mailer = new Swift_Mailer($transport);
+
+  $response = [];
+  if ($mailer->send($message)) {
+    $response['status'] = 'success';
+    $response['title'] = 'Сообщение отправлено!';
+    $response['message'] = 'Мы свяжемся с вами в ближайшее время!';
+  } else {
+    $response['status'] = 'error';
+    $response['title'] = 'Ошибка при отправке сообщения';
+    $response['message'] = 'Попробуйте отправить сообщение позже';
+  }
+
+  print json_encode($response);
 }
-?>
