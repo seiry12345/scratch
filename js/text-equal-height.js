88999667e35fcf0cdbf875134398b4eca66bf3b8
seiry12345 <35530095+seiry12345@users.noreply.github.com>
2019-03-13 10:38:49 +0600

    Update text-equal-height.js

--- js/text-equal-height.js
+++ js/text-equal-height.js
@@ -1,3 +1,19 @@
+function equalHeight(num, selector) {
+    var discountItems = num;
+
+    var items = $(selector);
+    for (var i = 0; i < items.length; i = i + discountItems) {
+      var row = items.slice(i, i + discountItems);
+      var max = 0;
+      $(row).each(function () {
+        if ($(this).height() > max) {
+          max = $(this).height();
+        }
+      });
+      $(row).height(max);
+    }
+  }
+
 if($(".front-news").length) {
         var catalogItems = 5;
         if ($(window).width() > 900) {
