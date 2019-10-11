7313b8761e166267dd518ab93ae3f008057bd4e2
seiry12345 <35530095+seiry12345@users.noreply.github.com>
2019-10-11 15:25:24 +0600

    Update trigger-href.js

--- js/trigger-href.js
+++ js/trigger-href.js
@@ -2,15 +2,19 @@ var href = $(this).find('h3 a').attr('href');
 window.location.href = href;
 
 // pseudo url on item
-    function targetHref(selectorContainer, selector) {
-      if ($(selectorContainer).length) {
-        let item = $(selector);
+    function targetHref(selector) {
+      const item = $(selector);
+      let href;
 
-        item.on('click', function () {
-          let href = $(this).find('h3 a').attr('href');
-          window.location.href = href;
-        });
-      }
+      item.on('click', function () {
+        if ($(this).find('h2 a').attr('href') !== 'undefined') {
+          href = $(this).find('h2 a').attr('href');
+        } else {
+          href = $(this).find('h3 a').attr('href');
+        }
+
+        window.location.href = href;
+      });
     }
 
     targetHref('.front-services', '.front-services .views-row');
