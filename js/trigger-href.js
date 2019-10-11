1b76a4a74ca333bcd92574798e85a0b92b99eebe
seiry12345 <35530095+seiry12345@users.noreply.github.com>
2019-10-11 15:20:21 +0600

    Update trigger-href.js

--- js/trigger-href.js
+++ js/trigger-href.js
@@ -1,2 +1,17 @@
 var href = $(this).find('h3 a').attr('href');
 window.location.href = href;
+
+// pseudo url on item
+    function targetHref(selectorContainer, selector) {
+      if ($(selectorContainer).length) {
+        let item = $(selector);
+
+        item.on('click', function () {
+          let href = $(this).find('h3 a').attr('href');
+          window.location.href = href;
+        });
+      }
+    }
+
+    targetHref('.front-services', '.front-services .views-row');
+    targetHref('.product-wrapper', '.view-product .views-row');
