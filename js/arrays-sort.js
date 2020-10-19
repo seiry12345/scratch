ae15364b95b12bb9f80191d466c48ce61608d98d
seiry12345 <35530095+seiry12345@users.noreply.github.com>
2020-10-19 21:29:57 +0600

    Update arrays-sort.js

--- js/arrays-sort.js
+++ js/arrays-sort.js
@@ -1,6 +1,6 @@
 // Сортировка по возрастанию
- function sortByAge(arr) {
-  arr.sort((a, b) => a.age > b.age ? 1 : -1);
+function sortByAsc(arr, prop) {
+    arr.sort((a, b) => a[prop] > b[prop] ? 1 : -1);
 }
 
 // Тасование Фишера — Йетса (https://ru.wikipedia.org/wiki/%D0%A2%D0%B0%D1%81%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%A4%D0%B8%D1%88%D0%B5%D1%80%D0%B0_%E2%80%94_%D0%99%D0%B5%D1%82%D1%81%D0%B0)
